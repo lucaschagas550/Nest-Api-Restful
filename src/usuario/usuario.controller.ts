08a09f8cd06a951686a66df46c211ab52f480a7f
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2023-10-11 19:20:27 -0300

    feat: crud

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { CriaUsuarioDTO } from "./dto/CriaUsuario.DTO";
+import { UsuarioEntity } from "./usuario.entity";
+import { v4 as uuid } from 'uuid';
+import { ListaUsuarioDTO } from "./dto/ListaUsuario.dto";
+import { AtualizaUsuarioDTO } from "./dto/AtualizaUsuarioDTO ";
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -11,12 +15,45 @@ export class UsuarioController {
 
     @Post()
     async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
-        this.usuarioRepository.salvar(dadosDoUsuario);
-        return dadosDoUsuario;
+        const usuarioEntity = new UsuarioEntity();
+        usuarioEntity.email = dadosDoUsuario.email;
+        usuarioEntity.senha = dadosDoUsuario.senha;
+        usuarioEntity.nome = dadosDoUsuario.nome;
+        usuarioEntity.id = uuid();
+
+        this.usuarioRepository.salvar(usuarioEntity);
+        return { usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome), message: 'Usuario criado com sucesso' };
     }
 
     @Get()
     async listUsuarios() {
-        return this.usuarioRepository.listar();
+        const usuariosSalvos = await this.usuarioRepository.listar();
+        const usuariosLista = usuariosSalvos.map(
+            usuario => new ListaUsuarioDTO(
+                usuario.id,
+                usuario.nome
+            )
+        );
+        return usuariosLista;
+    }
+
+    @Put('/:id')
+    async atualizaUsuario(@Param('id') id: string, @Body() novosDados: AtualizaUsuarioDTO) {
+        const usuarioAtualizado = await this.usuarioRepository.atualiza(id, novosDados);
+
+        return {
+            usuario: usuarioAtualizado,
+            mensagem: 'usuário atualizado com sucesso',
+        }
+    }
+
+    @Delete('/:id')
+    async removeUsuario(@Param('id') id: string) {
+        const usuarioRemovido = await this.usuarioRepository.remove(id);
+
+        return {
+            usuario: usuarioRemovido,
+            mensagem: 'usuário removido com sucesso'
+        }
     }
 }
--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { UsuarioEntity } from "./usuario.entity";
 
 @Injectable() //Anotacao para ser um provider
 export class UsuarioRepository {
-    private usuarios = [];
+    private usuarios: UsuarioEntity[] = [];
 
-    async salvar(usuario) {
+    async salvar(usuario: UsuarioEntity) {
         this.usuarios.push(usuario);
         console.log(this.usuarios);
     }
@@ -19,5 +20,46 @@ export class UsuarioRepository {
         );
 
         return possivelUsuario !== undefined;
+    }
+
+    //Partial faz com que todas propriedades da model fossem opcionais, assim n eh obrigatorio passar todos os dados
+    async atualiza(id: string, dadosDeAtualizacao: Partial<UsuarioEntity>) {
+        const possivelUsuario = this.buscaPorId(id);
+
+        if (!possivelUsuario) { // se nao encontrar um usuario gera uma execao
+            throw new Error('Usuário não existe');
+        }
+
+        //Mapeia as propriedades do objeto para substituir o valor
+        Object.entries(dadosDeAtualizacao).forEach(([chave, valor]) => {
+            if (chave === 'id') {
+                return;
+            }
+
+            possivelUsuario[chave] = valor;
+        });
+
+        return possivelUsuario;
+    }
+
+    async remove(id: string) {
+        const usuario = this.buscaPorId(id);
+        this.usuarios = this.usuarios.filter(
+            usuarioSalvo => usuarioSalvo.id !== id
+        );
+
+        return usuario;
+    }
+
+    private buscaPorId(id: string) {
+        const possivelUsuario = this.usuarios.find(
+            u => u.id === id
+        );
+
+        if (!possivelUsuario) {
+            throw new Error('Usuário não existe');
+        }
+
+        return possivelUsuario;
     }
 }
